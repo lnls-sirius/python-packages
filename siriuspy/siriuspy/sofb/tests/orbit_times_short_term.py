3d2f4573702fc5eb8decd1f86a89bf21b1a9cd9c
Sirius <sirius@debian>
2020-08-13 17:52:12 -0300

    SOFB.TST: Improve scripts.

--- siriuspy/siriuspy/sofb/tests/orbit_times_short_term.py
+++ siriuspy/siriuspy/sofb/tests/orbit_times_short_term.py
@@ -28,13 +28,6 @@ evt = Event()
 signal.signal(signal.SIGINT, shutdown)
 signal.signal(signal.SIGTERM, shutdown)
 
-tmpl = '{:^18s} '*4 + '\n'
-with open('orbx.txt', 'w') as fil:
-    fil.write(tmpl.format('# avg', 'std', 'max', 'min'))
-
-with open('orby.txt', 'w') as fil:
-    fil.write(tmpl.format('# avg', 'std', 'max', 'min'))
-
 print('creating orbit object...')
 orbit = EpicsOrbit('SI')
 orbit.set_orbit_acq_rate(30)
@@ -46,17 +39,15 @@ time.sleep(5)
 print('starting acquisition')
 tmpl = '{ave:18.9f} {std:18.9f} {maxi:18.9f} {mini:18.9f}\n'
 cnt = 0
+orbs = []
 while not evt.is_set():
-    orb = orbit.get_orbit(synced=True)
-    orb *= -1
-    datax = calc_values(orb[:160])
-    datay = calc_values(orb[160:])
-    with open('orbx.txt', 'a') as fil:
-        fil.write(tmpl.format(**datax))
-    with open('orby.txt', 'a') as fil:
-        fil.write(tmpl.format(**datay))
+    t0 = time.time()
+    orbs.append(orbit.get_orbit(synced=True))
     cnt += 1
-    if cnt > 35 * 60 * 25:
+    print(f'dt = {(time.time()-t0)*1000:.2f} ms')
+    if cnt > 40 * 25:
         break
+
+np.savetxt('orb.txt', orbs)
 
 orbit.shutdown()
